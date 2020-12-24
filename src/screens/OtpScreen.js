7cd27dc2803a02ad5e65a107c7d0fb8c69ecbac1
Ravi Kiran <ravi@greettech.com>
2020-12-24 19:04:44 +0530

    updating verify button

--- src/screens/OtpScreen.js
+++ src/screens/OtpScreen.js
@@ -3,10 +3,10 @@ import { StyleSheet } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { Content, Item, Input, View } from 'native-base';
 import { Grid, Col } from 'react-native-easy-grid';
-
+import Spacer from '../components/Spacer';
 class OtpScreen extends React.Component {
 
-    
+
     state = { otp: [] };
     otpDigit = [];
     componentDidMount() {
@@ -14,38 +14,50 @@ class OtpScreen extends React.Component {
     }
 
     render() {
-    const { params } = this.props.navigation.state;
-    console.log(params.phoneNumber);
+        const { params } = this.props.navigation.state;
+        console.log(params.phoneNumber);
         return (
             <Content>
                 <Text h2> Enter OTP Code</Text>
                 <Text h4> Pease verify your 4 digit OTP number </Text>
-                <Text h4 style={{marginRight:20}}>sent to {params.phoneNumber}</Text>
+                <Text h4 style={{ marginRight: 20 }}>sent to {params.phoneNumber}</Text>
                 <Grid style={styles.gridPad}>
                     {this.renderDigits()}
                 </Grid>
                 <View style={styles.resendContainer}>
-                <Text style={styles.otpErr}>Didn't receive code?</Text>
-                <Text style={styles.resendLink}>Resend</Text>
+                    <Text style={styles.otpErr}>Didn't receive code?</Text>
+                    <Text style={styles.resendLink}>Resend</Text>
+
+                </View>
+                <Spacer />
+                <Spacer />
+                <View style={styles.buttonContainer}>
+                <Button
+                    title="Verify OTP"
+                    style={styles.verifyButton}
+                />
                 </View>
                 
+
             </Content>
-            
+
         );
     }
 
     renderDigits() {
         const inputs = Array(4).fill(0);
         const txt = inputs.map(
-            (i, j) => <Col key={j} style={styles.txtMargin}><Item regular>
-                <Input
-                    style={[styles.inputRadius, { borderRadius: 10 }]}
-                    keyboardType="numeric"
-                    onChangeText={v => this.nextInput(j, v)}
-                    onKeyPress={e => this.prevInput(e.nativeEvent.key, j)}
-                    ref={ref => this.otpDigit[j] = ref}
-                />
-            </Item></Col>
+            (i, j) => <Col key={j} style={styles.txtMargin}>
+                <Item regular>
+                    <Input
+                        style={[styles.inputRadius, { borderRadius: 10 }]}
+                        keyboardType="numeric"
+                        onChangeText={v => this.nextInput(j, v)}
+                        onKeyPress={e => this.prevInput(e.nativeEvent.key, j)}
+                        ref={ref => this.otpDigit[j] = ref}
+                    />
+                </Item>
+            </Col>
         );
         return txt;
     }
@@ -64,14 +76,14 @@ class OtpScreen extends React.Component {
     }
 
     prevInput(key, index) {
-        if (key === 'Backspace' && index !== 0){
-             this.otpDigit[index - 1]._root.focus();
-            
+        if (key === 'Backspace' && index !== 0) {
+            this.otpDigit[index - 1]._root.focus();
+
         }
-        
+
     }
 
-    
+
 }
 const styles = StyleSheet.create({
     gridPad: {
@@ -84,17 +96,26 @@ const styles = StyleSheet.create({
         textAlign: 'center'
 
     },
-    resendContainer:{
-        marginLeft:20
+    resendContainer: {
+        marginLeft: 20
     },
-    otpErr:{
-        fontSize:20
+    otpErr: {
+        fontSize: 20
     },
-    resendLink:{
-        fontSize:20,
-        color:'#C76700',
-        position:"absolute",
-        marginLeft:180
+    resendLink: {
+        fontSize: 20,
+        color: '#C76700',
+        position: "absolute",
+        marginLeft: 180
+    },
+    buttonContainer: {
+        color:"#C76700",
+        marginLeft:20,
+        width:"40%"
+    },
+    verifyButton:{
+        
+        color: '#C76700',
     }
 
 });
